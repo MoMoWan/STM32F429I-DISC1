20e37e603dd5221da5a0b653f33e6e902f2f4d69
Vân Sơn <thanh.duong.vs@gmail.com>
2016-11-24 14:25:30 +0700

    update file led
    update file led

--- 01-USART/Utilities/stm32f4_led.h
+++ 01-USART/Utilities/stm32f4_led.h
@@ -5,20 +5,43 @@
 #endif
      
 #include "stm32f4xx.h"
-
+#include "stm32f4_gpio.h"
+	 
 #define LED_GREEN					GPIO_Pin_13
 #define LED_RED						GPIO_Pin_14
 #define LED_ALL						LED_GREEN | LED_RED
-
 #define LED_PORT					GPIOG
+
+#define BUTTON_PORT					GPIOA
+#define BUTTON_PIN					GPIO_Pin_0
+#define BUTTON_PRESSED				1
+#define BUTTON_PULL					GPIO_PuPd_DOWN
 	 
 void LED_Init(void);
-	 
 #define LED_On(led)					GPIO_SetBits(LED_PORT, led);
 #define LED_Off(led)				GPIO_ResetBits(LED_PORT, led);
 #define LED_Toggle(led)				GPIO_ToggleBits(LED_PORT, led);
 #define LED_IsOn(led)				(((LED_PORT)->ODR & (led)) == 0 ? 0 : 1)
 
+#define Button_Pressed()       ((GPIO_GetInputPinValue(BUTTON_PORT, BUTTON_PIN) == 0) != BUTTON_PRESSED)
+
+/**
+ * @brief  Checks if button was pressed now, but was not already pressed before
+ * @param  None
+ * @retval Button on pressed value
+ *           - 0: In case that button has been already pressed on last call or was not pressed at all yet
+ *           - > 0: Button was pressed, but state before was released
+ */
+uint8_t Button_OnPressed(void);
+
+/**
+ * @brief  Checks if button was released now, but was already pressed before
+ * @param  None
+ * @retval Button on released value
+ *           - 0: In case that button has been already released on last call or was not released at all yet
+ *           - > 0: Button was released, but state before was pressed
+ */
+uint8_t Button_OnReleased(void);
 	 
 #ifdef __cplusplus
 }
--- 04-ILI9341_BUTTON/Utilities/stm32f4_led.c
+++ 04-ILI9341_BUTTON/Utilities/stm32f4_led.c
@@ -1,5 +1,7 @@
 #include "stm32f4_led.h"
 
+static volatile uint8_t ButtonPressed = 0;
+
 void LED_Init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -12,4 +14,50 @@ void LED_Init(void)
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(LED_PORT, &GPIO_InitStructure);
+}
+
+void Button_Init(void)
+{
+	/* Set pin as input */
+	GPIO_Inits(BUTTON_PORT, BUTTON_PIN, GPIO_Mode_IN, GPIO_OType_PP, BUTTON_PULL, GPIO_Low_Speed);
+}
+
+uint8_t Button_OnPressed(void)
+{
+	/* If button is now pressed, but was not already pressed */
+	if (Button_Pressed()) {
+		if (!ButtonPressed) {
+			/* Set flag */
+			ButtonPressed = 1;
+			
+			/* Return button onpressed */
+			return 1;
+		}
+	} else {
+		/* Clear flag */
+		ButtonPressed = 0;
+	}
+	
+	/* Button is not pressed or it was already pressed before */
+	return 0;
+}
+
+uint8_t Button_OnReleased(void)
+{
+	/* If button is now released, but was not already released */
+	if (!Button_Pressed()) {
+		if (ButtonPressed) {
+			/* Set flag */
+			ButtonPressed = 0;
+			
+			/* Return button onreleased */
+			return 1;
+		}
+	} else {
+		/* Set flag */
+		ButtonPressed = 1;
+	}
+	
+	/* Button is not released or it was already released before */
+	return 0;
 }
